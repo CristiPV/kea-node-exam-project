15690cc6d78cd9fa934ddc455493717b4759bf61
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-05-23 21:18:45 +0200

    feature: add user selection to navbar, take name from it for username

--- src/public/home/sockets.js
+++ src/public/home/sockets.js
@@ -1,26 +1,31 @@
 const socket = io();
 
+let chatUser = "PlaceholderName";
+
+function empowerChat() {
+  const chatButtonToEmpower = document.getElementById("chat-button");
+  const chatToEmpower = document.getElementById("chat-input");
+
+  chatUser = document.getElementById("username-input").value;
+
+  chatButtonToEmpower.removeAttribute("disabled");
+  chatToEmpower.removeAttribute("disabled");
+}
+
 function submitChat() {
   // Retrieve the text from the page
   const chatText = document.getElementById("chat-input").value;
-  const chatUser = "peter piper";
-  if (chatText !== "") {
-    console.log("text to send:", chatText);
-    console.log("user to send:", chatUser);
-
     // Broadcast the text to be displayed
     socket.emit("submitChat", { chatText, chatUser });
 
     // Clear the input field for next chat
     document.getElementById("chat-input").value = "";
-  } else {
-    console.log("No text detected");
-  }
 }
 
 socket.on("updateChat", (data) => {
-  console.log("data received to update:", data);
-  const containerDiv = document.getElementsByClassName("chat-text-container")[0];
+  const containerDiv = document.getElementsByClassName(
+    "chat-text-container"
+  )[0];
 
   const chatDiv = document.createElement("div");
   chatDiv.classList.add("chat-text");
