19dd65b2149e5c45f3b99961bc08252911654e5d
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-05-23 15:37:11 +0200

    refactor: refactor app.js | feature: add required imports for socket

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const fs = require("fs");
-
+const http = require("htpp");
 const dotenvConfig = dotenv.config();
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 app.use(express.static("src/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Components
+// Files
 const navbar = fs.readFileSync(
   __dirname + "/public/components/navbar/navbar.html"
 );
+
+const home = fs.readFileSync(__dirname + "/public/screens/home/home.html");
+
 const footer = fs.readFileSync(
   __dirname + "/public/components/footer/footer.html"
 );
-// Screens
-const home = fs.readFileSync(__dirname + "/public/screens/home/home.html");
 
-const PORT = process.env.PORT || 3000;
+// Sockets
 
+
+// Routes
 app.get("/", (req, res) => {
   res.send(navbar + home + footer);
 });
 
-const server = app.listen(PORT, (error) => {
+// App server setup
+const PORT = process.env.PORT || 3000;
+
+const appServer = app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
   }
@@ -34,5 +42,5 @@ const server = app.listen(PORT, (error) => {
     console.log(".env error:", dotenvConfig.error);
   }
 
-  console.log("Server started on port:", server.address().port);
+  console.log("Server started on port:", appServer.address().port);
 });
