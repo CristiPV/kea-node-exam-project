5964b5388eb46802aa28924fcc3b5c7eef40ea98
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-05-26 12:30:26 +0200

    documentation: add basic creation script

---
+++ docs/DB-creation-script.sql
@@ -0,0 +1,54 @@
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `drawtastic` DEFAULT CHARACTER SET utf8 ;
+USE `drawtastic` ;
+
+-- -----------------------------------------------------
+-- Table mydb.draw_option
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`draw_option` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
+
+
+-- -----------------------------------------------------
+-- Table mydb.draw_option
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`draw_option` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
+
+
+-- -----------------------------------------------------
+-- Table mydb.drawing
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`drawing` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `artist` VARCHAR(45) NOT NULL,
+  `winner` VARCHAR(45) NULL,
+  `time` DATETIME NOT NULL,
+  `image` BLOB NOT NULL,
+  `draw_option_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_drawing_draw_option_idx` (`draw_option_id` ASC) VISIBLE,
+  CONSTRAINT `fk_drawing_draw_option`
+    FOREIGN KEY (`draw_option_id`)
+    REFERENCES `mydb`.`draw_option` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
+
+
