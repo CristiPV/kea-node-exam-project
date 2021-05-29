7d683389abf760d4a6faf9119a81302c169c6f6e
Alex M <57399961+IroniX2@users.noreply.github.com>
2021-05-29 20:48:29 +0200

    feature: make drawings get also image data

--- src/routes/drawing.js
+++ src/routes/drawing.js
@@ -2,6 +2,39 @@ const router = require("express").Router();
 const pool = require("../mysql/mysql").pool;
 
 router.get("/api/drawings", (req, res) => {
+  let finalResult;
+  // Get a connection from the pool
+  pool.getConnection((error, connection) => {
+    if (error) throw error;
+
+    // Query 1: get all text based values and add them to the array
+    connection.query(
+      "SELECT drawing.artist, drawing.winner, drawing.time, draw_option.name FROM drawing LEFT JOIN draw_option ON drawing.draw_option_id = draw_option.id;",
+      (error, result) => {
+        if (error) throw error;
+
+        finalResult = JSON.parse(JSON.stringify(result));
+      }
+    );
+    // Query 2: get all the image blobs, as utf8 strings, and add them to the array
+    connection.query(
+      "SELECT CONVERT (image USING utf8) AS image FROM drawing;",
+      (error, result) => {
+        if (error) throw error;
+
+        finalResult.forEach((element, index) => {
+          element.image = JSON.parse(JSON.stringify(result[index].image));
+        });
+
+        res.send({ drawings: finalResult });
+      }
+    );
+    // Release connection
+    connection.release();
+  });
+});
+
+/*
   pool.query(
     "SELECT drawing.artist, drawing.winner, drawing.time, drawing.image, draw_option.name FROM drawing LEFT JOIN draw_option ON drawing.draw_option_id = draw_option.id",
     (error, result) => {
@@ -11,7 +44,19 @@ router.get("/api/drawings", (req, res) => {
       res.send({ drawings: result });
     }
   );
-});
+
+
+pool.query(
+  "select image using utf8) from drawing;",
+  (error, result) => {
+    if (error) throw error;
+    result = JSON.parse(JSON.stringify(result));
+
+    res.send({ drawings: result });
+  }
+);
+*/
+
 /*
 router.post("/api/drawings", (req, res) => {
   const artist = "yo";
