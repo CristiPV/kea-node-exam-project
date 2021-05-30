833c53b5b0480cb2b8f4b2472ea135993d4ef9d2
alex <57399961+IroniX2@users.noreply.github.com>
2021-05-30 11:34:40 +0200

    refactor: alter the history.html divs to better fit presentation + information flow

--- src/public/history/history.js
+++ src/public/history/history.js
@@ -27,45 +27,92 @@ async function getHistory() {
     // Formatting date
     formattedDate = formatDate(drawingData.time);
 
-    // Div to contain text and image
+    // Div to contain both text and image
     const imageTextCombo = document.createElement("div");
     imageTextCombo.classList.add("image-text-combo");
 
+    // ---- Image ---
     // Child div to contain image
     const imageInformation = document.createElement("div");
     imageInformation.classList.add("image-information");
-
+    // Image
     const image = document.createElement("img");
     image.classList.add("raw-image");
     image.src = drawingData.image;
     // Add image element to parent div
     imageInformation.appendChild(image);
 
+    // ---- Text ----
     // Add child div to contain text
     const textInformation = document.createElement("div");
     textInformation.classList.add("text-information");
 
+    // Container for artist
+    const artistDiv = document.createElement("div");
+    artistDiv.classList.add("artist-container");
+    // Artist presentation
+    const artistPresentation = document.createElement("span");
+    artistPresentation.classList.add("game-presentation");
+    artistPresentation.innerText = "Artist : ";
+    // Artist presentation
     const artistName = document.createElement("span");
     artistName.classList.add("game-information");
-    artistName.innerText = "Artist name \t: " + drawingData.artist;
-
+    artistName.innerText = drawingData.artist;
+    // Add presentation + information to the container
+    artistDiv.appendChild(artistPresentation);
+    artistDiv.appendChild(artistName)
+    
+    // Container for winner
+    const winnerDiv = document.createElement("span");
+    winnerDiv.classList.add("winner-container")
+    // Winner presentation
+    const winnerPresentation = document.createElement("span");
+    winnerPresentation.classList.add("game-presentation");
+    winnerPresentation.innerText = "Winner : ";
+    // Winner information
     const winnerName = document.createElement("span");
     winnerName.classList.add("game-information");
-    winnerName.innerText = "Winner name : " + drawingData.winner;
+    winnerName.innerText = drawingData.winner;
+     // Add presentation + information to the container
+    winnerDiv.appendChild(winnerPresentation);
+    winnerDiv.appendChild(winnerName);
 
+    // Container for time
+    const timeDiv = document.createElement("div");
+    timeDiv.classList.add("time-container");
+    // Time presentation
+    const timePresentation = document.createElement("span");
+    timePresentation.classList.add("game-presentation");
+    timePresentation.innerText = "Time won : ";
+    // Time information
     const timeWon = document.createElement("span");
     timeWon.classList.add("game-information");
-    timeWon.innerText = "Time of win: " + formattedDate;
+    timeWon.innerText = formattedDate;
+    // Add presentation + information to the container
+    timeDiv.appendChild(timePresentation);
+    timeDiv.appendChild(timeWon);
 
+    // Container for drawing
+    const drawingIdDiv = document.createElement("div");
+    drawingIdDiv.classList.add("drawing-id-div");
+    // Drawing presentation
+    const drawingPresentation = document.createElement("span");
+    drawingPresentation.classList.add("game-presentation");
+    drawingPresentation.innerText = "Drawing name : ";
+    // Drawing information
     const drawingId = document.createElement("span");
     drawingId.classList.add("game-information");
-    drawingId.innerText = "Drawing name: " + drawingData.name;
+    drawingId.innerText = drawingData.name;
+    // Add presentation + information to the container
+    drawingIdDiv.appendChild(drawingPresentation);
+    drawingIdDiv.appendChild(drawingId);
 
-    // Add text to parent div
-    textInformation.appendChild(artistName);
-    textInformation.appendChild(winnerName);
-    textInformation.appendChild(timeWon);
-    textInformation.appendChild(drawingId);
+
+    textInformation.appendChild(artistDiv)
+    textInformation.appendChild(winnerDiv)
+    textInformation.appendChild(timeDiv);
+    textInformation.appendChild(drawingIdDiv);
+
 
     // Add both div children to parent div
     imageTextCombo.appendChild(imageInformation);
